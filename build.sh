6439b0dc7a7ed60a972eb874abd65d57b87350d1
Michał Gapiński <mikegapinski@users.noreply.github.com>
2023-07-17 22:34:31 +0200

    Build OTA package

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-#!/bin/bash -e
+#!/bin/bash
 
 echo Building the Android
 pushd aosptree
 . build/envsetup.sh
-# lunch vim3-userdebug
-lunch radxa_zero-userdebug
+lunch tesla_android_radxa_zero-userdebug
 make images -k || make images -j1
 make sdcard
+make otapackage
 popd
